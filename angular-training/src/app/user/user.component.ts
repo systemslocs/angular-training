d3c5836cc0371ae406a4b48943a96096b96e7e50
Locs Systems <systemslocs@gmail.com>
2024-08-06 09:28:19 -0300

    estrutura inicial, header e user.

---
+++ angular-training/src/app/user/user.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input } from '@angular/core';
+
+//const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+    //selectedUser = signal(DUMMY_USERS[randomIndex]);
+    //imagePath = computed(() =>'assets/users/' + this.selectedUser().avatar)
+
+    //get imagePath(){
+    //    return 'assets/users/' + this.selectedUser.avatar
+    //}
+    @Input() avatar!: string;
+    @Input() name!: string;
+
+    get imagePath(){
+        return 'assets/users/' + this.avatar;
+    }
+
+    onSelectUser(){
+        //const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+        //this.selectedUser.set(DUMMY_USERS[randomIndex]) ;
+    }
+}
